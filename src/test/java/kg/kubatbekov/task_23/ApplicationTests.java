0839c36fabbc3856a42db19d181f796335337718
Kubatbekov Nurlan <kubatbekovnurlan13@gmail.com>
2023-03-01 06:50:51 +0600

    First commit: basic of project

---
+++ src/test/java/kg/kubatbekov/task_23/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.kubatbekov.task_23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
