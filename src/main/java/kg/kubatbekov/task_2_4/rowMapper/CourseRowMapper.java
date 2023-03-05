4740b33cc0c5ec0ff436df9c8719ec6039f53997
Kubatbekov Nurlan <kubatbekovnurlan13@gmail.com>
2023-03-06 02:12:33 +0600

    Fourth commit: utilized implemented functionality in right way and added some log

---
+++ src/main/java/kg/kubatbekov/task_2_4/rowMapper/CourseRowMapper.java
@@ -0,0 +1,17 @@
+package kg.kubatbekov.task_2_4.rowMapper;
+
+import kg.kubatbekov.task_2_4.model.Course;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CourseRowMapper implements RowMapper<Course> {
+    @Override
+    public Course mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Course(
+                rs.getInt("course_id"),
+                rs.getString("course_name"),
+                rs.getString("course_description"));
+    }
+}
